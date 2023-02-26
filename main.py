40dd780d5814ddac9bd2497565654923b7a2dc8a
Dmitry <68408042+FonDerMark@users.noreply.github.com>
2023-02-26 13:06:35 +0500

    0.0.3

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
 import eel
-import requests
-from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
-import lxml
 
-# eel.init('web')
 
-url = 'https://hubblesite.org/resource-gallery/images'
+eel.init('web')
 
 
 @eel.expose
 def test():
-    ua = UserAgent()
-    headers = {
-        'User-Agent': ua.random,
-    }
-    response = requests.get(url, headers=headers).content
-    soup = BeautifulSoup(response, 'html.parser')
-    result = soup.findAll('picture')
-    for i in result:
-        print(i)
-    return soup.prettify()
+    return 'Some text'
 
 
-# eel.start('main.html', size=(400, 400))
-
-if __name__ == '__main__':
-    test()
+eel.start('main.html', size=(400, 400))
